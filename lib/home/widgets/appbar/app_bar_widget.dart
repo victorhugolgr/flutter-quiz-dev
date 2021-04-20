c5878253d0a03839f9e489a9221cb0c19a6a014a
Victor Hugo <victorhugolgr@gmail.com>
2021-04-20 16:14:36 -0300

    Create app bar

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../core/core.dart';
+import '../score_card/score_card_widget.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget()
@@ -8,36 +9,46 @@ class AppBarWidget extends PreferredSize {
             preferredSize: Size.fromHeight(250),
             child: Container(
               height: 250,
-              decoration: BoxDecoration(gradient: AppGradients.linear),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text.rich(
-                      TextSpan(
-                        text: "Olá, ",
-                        style: AppTextStyles.title,
-                        children: [
+              child: Stack(
+                children: [
+                  Container(
+                    height: 161,
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(
                           TextSpan(
-                              text: "Victor Hugo",
-                              style: AppTextStyles.titleBold),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      width: 58,
-                      height: 58,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              "https://avatars.githubusercontent.com/u/5514551?v=4"),
+                            text: "Olá, ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                  text: "Victor Hugo",
+                                  style: AppTextStyles.titleBold),
+                            ],
+                          ),
                         ),
-                      ),
-                    )
-                  ],
-                ),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://avatars.githubusercontent.com/u/5514551?v=4"),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(0.0, 1.0),
+                    child: ScoreCardWidget(),
+                  )
+                ],
               ),
             ));
 }
