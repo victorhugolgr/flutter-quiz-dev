7e160e0b5e058a341eafb5ef8b03e5caa8ec8829
Victor Hugo <victorhugolgr@gmail.com>
2021-04-22 09:36:29 -0300

    create data structure

---
+++ lib/shared/models/question_model.dart
@@ -0,0 +1,11 @@
+import 'awnser_model.dart';
+
+class QuestionModel {
+  final String title;
+  final List<AwnserModel> awnsers;
+
+  QuestionModel({
+    required this.title,
+    required this.awnsers,
+  }) : assert(awnsers.length == 4);
+}
